2e68119ca74cd306e4f8c93dff0f9fa115093e72
tspry <prasannatapol@gmail.com>
2024-12-24 17:32:49 +0545

    Refactored

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ print_markdown(
     "### Thanks for using this tool! Feel free to contribute to this project on GitHub! If you have any questions, feel free to join my Discord server or submit a GitHub issue. You can find solutions to many common problems in the documentation: https://reddit-video-maker-bot.netlify.app/"
 )
 
+
 def main(POST_ID=None) -> None:
     global redditid, reddit_object
     reddit_object = get_subreddit_threads(POST_ID)
@@ -74,8 +75,6 @@ def shutdown() -> NoReturn:
     print("Exiting...")
     sys.exit()
 
-
-# SO THAT THE SCRIPT CAN ALSO BE RUN FROM THE ENDPOINT
 def gen():
     ffmpeg_install()
     directory = Path().absolute()
@@ -127,50 +126,4 @@ def gen():
 
 
 if __name__ == "__main__":
-    ffmpeg_install()
-    directory = Path().absolute()
-    config = settings.check_toml(
-        f"{directory}/utils/.config.template.toml", f"{directory}/config.toml"
-    )
-    config is False and sys.exit()
-
-    if (
-        not settings.config["settings"]["tts"]["tiktok_sessionid"]
-        or settings.config["settings"]["tts"]["tiktok_sessionid"] == ""
-    ) and config["settings"]["tts"]["voice_choice"] == "tiktok":
-        print_substep(
-            "TikTok voice requires a sessionid! Check our documentation on how to obtain one.",
-            "bold red",
-        )
-        sys.exit()
-    try:
-        if config["reddit"]["thread"]["post_id"]:
-            for index, post_id in enumerate(
-                config["reddit"]["thread"]["post_id"].split("+")
-            ):
-                index += 1
-                print_step(
-                    f'on the {index}{("st" if index % 10 == 1 else ("nd" if index % 10 == 2 else ("rd" if index % 10 == 3 else "th")))} post of {len(config["reddit"]["thread"]["post_id"].split("+"))}'
-                )
-                main(post_id)
-                Popen("cls" if name == "nt" else "clear", shell=True).wait()
-        elif config["settings"]["times_to_run"]:
-            run_many(config["settings"]["times_to_run"])
-        else:
-            main()
-    except KeyboardInterrupt:
-        shutdown()
-    except ResponseException:
-        print_markdown("## Invalid credentials")
-        print_markdown("Please check your credentials in the config.toml file")
-        shutdown()
-    except Exception as err:
-        config["settings"]["tts"]["tiktok_sessionid"] = "REDACTED"
-        config["settings"]["tts"]["elevenlabs_api_key"] = "REDACTED"
-        print_step(
-            f"Sorry, something went wrong with this version! Try again, and feel free to report this issue at GitHub or the Discord community.\n"
-            f"Version: {__VERSION__} \n"
-            f"Error: {err} \n"
-            f'Config: {config["settings"]}'
-        )
-        raise err
+    gen()
